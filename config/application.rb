ed9a3dddadb00b659abc823cde56318140dbd575
Ariel Santos <ariel.santos@dcc.ufmg.br>
2021-03-27 00:28:42 -0300

    Tela de View para User, add I18n

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,13 @@ require_relative 'boot'
 require 'rails'
 # Pick the frameworks you want:
 require 'active_model/railtie'
-require 'active_job/railtie'
+# require 'active_job/railtie'
 require 'active_record/railtie'
 # require 'active_storage/engine'
 require 'action_controller/railtie'
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
-require 'action_text/engine'
+# require 'action_text/engine'
 require 'action_view/railtie'
 # require "action_cable/engine"
 require 'rails/test_unit/railtie'
@@ -32,5 +32,7 @@ module LeituraDoLugar
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = :'pt-BR'
+    config.i18n.default_locale    = :'pt-BR'
   end
 end
