15a40579080d88a0d931bb8c47b936cef4e0ffd7
Ariel Santos <ariel.santos@dcc.ufmg.br>
2021-03-29 12:54:30 -0300

    Configurando estilo padrão para forms

--- postcss.config.js
+++ postcss.config.js
@@ -3,10 +3,13 @@ module.exports = {
         require('postcss-import'),
         require('postcss-flexbugs-fixes'),
         require('postcss-preset-env')({
+            stage: 3,
             autoprefixer: {
                 flexbox: 'no-2009'
             },
-            stage: 3
+            features: {
+                'nesting-rules': true
+            }
         }),
         require('@tailwindcss/jit'),
         require('autoprefixer')
