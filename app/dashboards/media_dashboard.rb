f9a14ac7bfcf5d94d1b496f8d8c82b64db19be6f
Ariel Santos <ariel.santos@dcc.ufmg.br>
2021-07-22 00:31:42 -0300

    Corrige remoção de Mídias aninhadas e remove referências a gem administrate-field-shrine

--- app/dashboards/media_dashboard.rb
+++ app/dashboards/media_dashboard.rb
@@ -13,7 +13,7 @@ class MediaDashboard < Administrate::BaseDashboard
     name: Field::String,
     description: Field::Text,
     inline: Field::Boolean,
-    file: Field::Shrine,
+    file: Field::Deferred,
     created_at: Field::DateTime,
     updated_at: Field::DateTime
   }.freeze
